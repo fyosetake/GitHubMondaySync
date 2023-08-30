6087cd06d6fa5319701b01f21759c46c15c844a2
fyosetake <fyosetake@gmail.com>
2023-08-30 15:15:47 -0300

    implementar teste 'apiMondayHandler.test.js' para adequar ao novo escopo

--- tests/apiMondayHandler.test.js
+++ tests/apiMondayHandler.test.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { createTaskMondayReviewPullRequest } = require('../src/monday');
+const { createTaskMondayReviewPullRequest, updateTaskMondayReviewPullRequest } = require('../src/apiMondayHandler');
 
 jest.mock('axios');
 
@@ -17,6 +17,41 @@ describe('createTaskMondayReviewPullRequest', () => {
         axios.post.mockResolvedValue(expectedResponse);
 
         const result = await createTaskMondayReviewPullRequest(eventData);
+
+        expect(result).toEqual(expectedResponse.data);
+        expect(axios.post).toHaveBeenCalledWith(
+            "https://api.monday.com/v2",
+            expect.anything(),
+            expect.anything()
+        );
+    });
+
+    test('Deve lançar um erro em caso de falha', async () => {
+        const eventData = {
+            repository: {
+                full_name: 'fyosetake/teste',
+            },
+            number: '1',
+        };
+
+        const errorMessage = 'Erro simulado';
+
+        axios.post.mockRejectedValue(new Error(errorMessage));
+
+        await expect(createTaskMondayReviewPullRequest(eventData)).rejects.toThrowError('Erro ao criar a tarefa no Monday.com: ' + errorMessage);
+    });
+});
+
+describe('updateTaskMondayReviewPullRequest', () => {
+    test('Deve atualizar uma tarefa no Monday.com', async () => {
+        let htmlUrlGitHubPr = 'http://localhost';
+        let itemId = 1000;
+
+        const expectedResponse = { data: { id: 'mocked-task-id' } };
+
+        axios.post.mockResolvedValue(expectedResponse);
+
+        const result = await updateTaskMondayReviewPullRequest(itemId, htmlUrlGitHubPr);
 
         expect(result).toEqual(expectedResponse.data);
         expect(axios.post).toHaveBeenCalledWith(
